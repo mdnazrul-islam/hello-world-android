b12c7b9f0a8d2a4ba5e61781d26053baa9923934
Md Nazrul Islam <109346501+mdnazrul-islam@users.noreply.github.com>
2026-01-09 23:59:48 +0600

    Add MainActivity class for Hello World app

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
